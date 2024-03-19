abb16031eac80aa837f6ca91b6e65ffad5d49558
Quentin Gibson <quentingibson94@gmail.com>
2024-03-19 01:54:31 -0400

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{ts,tsx,js,jsx,mdx}",
+    "./src/components/**/*.{ts,tsx,js,jsx,mdx}",
+    "./src/slices/**/*.{ts,tsx,js,jsx,mdx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
